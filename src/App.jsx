581dd179963da0911fb4e29894bd21cfbf4d4b07
SachinKinha <sachinkinha78@gmail.com>
2023-10-18 14:36:31 +0530

    body add for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import ChatButton from "./components/ChatButton";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div>
       <Header />
+      <Body />
       <Footer />
       <ChatButton />
     </div>
---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div>
+      <h2>Hello Developer Please Develope me with your skills</h2>
+    </div>
+  )
+}
+
+export default Body
